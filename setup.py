fb2d242651d4cc6f42f12d47f4077853289487b6
David Lopez <david@labjack.com>
2013-02-07 13:21:28 -0700

    Corrected url in setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(name='LJMPython',
       version='0.8.1',
       description='The LJM Python module.',
-      url='http://www.labjack.com/',
+      url='http://labjack.com/support/ljm/examples/python',
       author='LabJack Corporation',
       license='X11',
       packages=['labjack', 'labjack.ljm']
