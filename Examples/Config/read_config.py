94c39bbc06ad1a6b5ff9df133c0c367631c32827
David Lopez <david@labjack.com>
2013-11-22 15:35:53 -0700

    Corrected a comment in the example.

--- Examples/Config/read_config.py
+++ Examples/Config/read_config.py
@@ -15,7 +15,7 @@ print("Opened a LabJack with Device type: %i, Connection type: %i,\n" \
     "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" % \
     (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
-# Setup and call eReadNames to read ethernet config. values from the LabJack.
+# Setup and call eReadNames to read config. values from the LabJack.
 names = ["PRODUCT_ID", "HARDWARE_VERSION", "FIRMWARE_VERSION",
          "BOOTLOADER_VERSION", "WIFI_VERSION", "SERIAL_NUMBER",
          "POWER_ETHERNET_DEFAULT", "POWER_WIFI_DEFAULT", "POWER_AIN_DEFAULT",
