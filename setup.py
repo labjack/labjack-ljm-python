57837160eace4e2d9cbf0b1bde22ef83b1cc89b4
David Lopez <david@labjack.com>
2015-05-21 17:17:12 -0600

    Updating files for release.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='LJMPython',
-      version='0.9.2',
+      version='0.9.3',
       description='The LJM Python package.',
       url='http://labjack.com/support/ljm/examples/python',
       author='LabJack Corporation',
