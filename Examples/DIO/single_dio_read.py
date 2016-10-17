9815cac295bd95a21a68ecf5545a61485cfe0344
David Lopez <david@labjack.com>
2016-10-17 17:45:05 -0600

    Updated digital I/O examples with T4 support.

--- Examples/DIO/single_dio_read.py
+++ Examples/DIO/single_dio_read.py
@@ -6,16 +6,27 @@ Demonstrates how to read a single digital input/output.
 from labjack import ljm
 
 # Open first found LabJack
-handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")
-#handle = ljm.openS("ANY", "ANY", "ANY")
+handle = ljm.openS("ANY", "ANY", "ANY")  # Any device, Any connection, Any identifier
+#handle = ljm.openS("T7", "ANY", "ANY")  # T7 device, Any connection, Any identifier
+#handle = ljm.openS("T4", "ANY", "ANY")  # T4 device, Any connection, Any identifier
+#handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")  # Any device, Any connection, Any identifier
 
 info = ljm.getHandleInfo(handle)
-print("Opened a LabJack with Device type: %i, Connection type: %i,\n" \
-    "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" % \
-    (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
+print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
+      "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
+      (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
-# Setup and call eReadName to read the DIO state from the LabJack.
-name = "FIO0"
+deviceType = info[0]
+
+# Setup and call eReadName to read a DIO state from the LabJack.
+if deviceType == ljm.constants.dtT4:
+    # Reading from FIO4 on the LabJack T4. FIO0-FIO3 are reserved for AIN0-AIN3.
+    # Note: Reading a single digital I/O will change the line from analog to
+    # digital input.
+    name = "FIO4"
+else:
+    # Reading from FIO0 on the LabJack T7 and other devices.
+    name = "FIO0"
 result = ljm.eReadName(handle, name)
 
 print("\n%s state : %f" % (name, result))
--- Examples/DIO/single_dio_write.py
+++ Examples/DIO/single_dio_write.py
@@ -2,23 +2,37 @@
 Demonstrates how to set a single digital state on a LabJack.
 
 """
-
 from labjack import ljm
 
 # Open first found LabJack
-handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")
-#handle = ljm.openS("ANY", "ANY", "ANY")
+handle = ljm.openS("ANY", "ANY", "ANY")  # Any device, Any connection, Any identifier
+#handle = ljm.openS("T7", "ANY", "ANY")  # T7 device, Any connection, Any identifier
+#handle = ljm.openS("T4", "ANY", "ANY")  # T4 device, Any connection, Any identifier
+#handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")  # Any device, Any connection, Any identifier
 
 info = ljm.getHandleInfo(handle)
-print("Opened a LabJack with Device type: %i, Connection type: %i,\n" \
-    "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" % \
-    (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
+print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
+      "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
+      (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
-# Setup and call eWriteName to set the DIO state on the LabJack.
-name = "FIO0"
-state = 0
+deviceType = info[0]
+
+# Setup and call eWriteName to set a DIO state on the LabJack.
+if deviceType == ljm.constants.dtT4:
+    # Setting FIO4 on the LabJack T4. FIO0-FIO3 are reserved for AIN0-AIN3.
+    name = "FIO4"
+
+    # If the FIO/EIO line is an analog input, it needs to first be changed to a
+    # digital I/O by reading from the line or setting it to digital I/O with the
+    # DIO_ANALOG_ENABLE register.
+    # For exanple:
+    #ljm.eReadName(handle, name)
+else:
+    # Setting FIO0 on the LabJack T7 and other devices.
+    name = "FIO0"
+
+state = 0  # Output state = low (0 = low, 1 = high)
 ljm.eWriteName(handle, name, state)
-
 print("\nSet %s state : %f" % (name, state))
 
 # Close handle
