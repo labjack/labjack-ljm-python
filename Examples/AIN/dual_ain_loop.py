5ce4621bf3273fa278fff34c9b223e29415ebe14
David Lopez <david@labjack.com>
2016-10-17 16:43:31 -0600

    Updated AIN examples for T7 and T4 support.

--- Examples/AIN/dual_ain_loop.py
+++ Examples/AIN/dual_ain_loop.py
@@ -2,58 +2,72 @@
 Demonstrates reading 2 analog inputs (AINs) in a loop from a LabJack.
 
 """
-
 from labjack import ljm
-import time
 import sys
+import time
+
+loopMessage = ""
+if len(sys.argv) > 1:
+    # An argument was passed. The first argument specifies how many times to
+    # loop.
+    try:
+        loopAmount = int(sys.argv[1])
+    except:
+        raise Exception("Invalid first argument \"%s\". This specifies how many"
+                        " times to loop and needs to be a number." %
+                        str(sys.argv[1]))
+else:
+    # An argument was not passed. Loop an infinite amount of times.
+    loopAmount = "infinite"
+    loopMessage = " Press Ctrl+C to stop."
 
 # Open first found LabJack
-handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")
-#handle = ljm.openS("ANY", "ANY", "ANY")
+handle = ljm.openS("ANY", "ANY", "ANY")  # Any device, Any connection, Any identifier
+#handle = ljm.openS("T7", "ANY", "ANY")  # T7 device, Any connection, Any identifier
+#handle = ljm.openS("T4", "ANY", "ANY")  # T4 device, Any connection, Any identifier
+#handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")  # Any device, Any connection, Any identifier
 
 info = ljm.getHandleInfo(handle)
-print("Opened a LabJack with Device type: %i, Connection type: %i,\n" \
-    "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" % \
-    (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
+print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
+      "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
+      (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
-# Setup and call eWriteNames to configure AINs on the LabJack.
-numFrames = 6
-names = ["AIN0_NEGATIVE_CH", "AIN0_RANGE", "AIN0_RESOLUTION_INDEX",
-         "AIN1_NEGATIVE_CH", "AIN1_RANGE", "AIN1_RESOLUTION_INDEX"]
-aValues = [199, 10, 0,
-           199, 10, 0]
+deviceType = info[0]
+
+# Setup and call eWriteNames to configure AIN0 and AIN1 on the LabJack.
+numFrames = 8
+# AIN0 and AIN1:
+#   Negative channel = single ended (199)
+#   Range: +/-10.0 V (10.0). T4 note: Only AIN0-AIN3 can support +/-10 V range.
+#   Resolution index = Default (0)
+#   Settling, in microseconds = Auto (0)
+names = ["AIN0_NEGATIVE_CH", "AIN0_RANGE", "AIN0_RESOLUTION_INDEX", "AIN0_SETTLING_US",
+         "AIN1_NEGATIVE_CH", "AIN1_RANGE", "AIN1_RESOLUTION_INDEX", "AIN1_SETTLING_US"]
+aValues = [199, 10.0, 0, 0,
+           199, 10.0, 0, 0]
+
 ljm.eWriteNames(handle, numFrames, names, aValues)
 
 print("\nSet configuration:")
 for i in range(numFrames):
     print("    %s : %f" % (names[i], aValues[i]))
 
-# Setup and call eReadNames to read AINs from the LabJack.
+# Read AIN0 and AIN1 from the LabJack with eReadNames in a loop.
 numFrames = 2
 names = ["AIN0", "AIN1"]
 
-s = ""
-if len(sys.argv) > 1:
-    #An argument was passed. The first argument specfies how many times to loop.
-    try:
-        loopAmount = int(sys.argv[1])
-    except:
-        raise Exception("Invalid first argument \"%s\". This specifies how many " \
-                        "times to loop and needs to be a number." % str(sys.argv[1]))
-else:
-    #An argument was not passed. Loop an infinite amount of times.
-    loopAmount = float("inf")
-    s = " Press Ctrl+C to stop."
-
-print("\nStarting %s read loops.%s" % (str(loopAmount), s))
-delay = 1 #delay between readings (in sec)
+print("\nStarting %s read loops.%s\n" % (str(loopAmount), loopMessage))
+delay = 1.0  # Delay between readings (in seconds)
 i = 0
-while i < loopAmount:
+while True:
     try:
         results = ljm.eReadNames(handle, numFrames, names)
-        print("\nAIN0 : %f V, AIN1 : %f V" % (results[0], results[1]))
+        print("AIN0 : %f V, AIN1 : %f V" % (results[0], results[1]))
         time.sleep(delay)
-        i = i + 1
+        if loopAmount is not "infinite":
+            i = i + 1
+            if i >= loopAmount:
+                break
     except KeyboardInterrupt:
         break
     except Exception:
--- Examples/AIN/single_ain_with_config.py
+++ Examples/AIN/single_ain_with_config.py
@@ -2,29 +2,35 @@
 Demonstrates configuring and reading a single analog input (AIN) with a LabJack.
 
 """
-
 from labjack import ljm
 
 # Open first found LabJack
-handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")
-#handle = ljm.openS("ANY", "ANY", "ANY")
+handle = ljm.openS("ANY", "ANY", "ANY")  # Any device, Any connection, Any identifier
+#handle = ljm.openS("T7", "ANY", "ANY")  # T7 device, Any connection, Any identifier
+#handle = ljm.openS("T4", "ANY", "ANY")  # T4 device, Any connection, Any identifier
+#handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")  # Any device, Any connection, Any identifier
 
 info = ljm.getHandleInfo(handle)
-print("Opened a LabJack with Device type: %i, Connection type: %i,\n" \
-    "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" % \
-    (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
+print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
+      "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
+      (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
-# Setup and call eWriteNames to configure the AIN on the LabJack.
-numFrames = 3
-names = ["AIN0_NEGATIVE_CH", "AIN0_RANGE", "AIN0_RESOLUTION_INDEX"]
-aValues = [199, 10, 0]
+# Setup and call eWriteNames to configure AIN0 on the LabJack.
+numFrames = 4
+# AIN0:
+#   Negative channel = single ended (199)
+#   Range: +/-10.0 V (10.0). T4 note: Only AIN0-AIN3 can support +/-10 V range.
+#   Resolution index = Default (0)
+#   Settling, in microseconds = Auto (0)
+names = ["AIN0_NEGATIVE_CH", "AIN0_RANGE", "AIN0_RESOLUTION_INDEX", "AIN0_SETTLING_US"]
+aValues = [199, 10, 0, 0]
 ljm.eWriteNames(handle, numFrames, names, aValues)
 
 print("\nSet configuration:")
 for i in range(numFrames):
     print("    %s : %f" % (names[i], aValues[i]))
 
-# Setup and call eReadName to read an AIN from the LabJack.
+# Setup and call eReadName to read AIN0 from the LabJack.
 name = "AIN0"
 result = ljm.eReadName(handle, name)
 
