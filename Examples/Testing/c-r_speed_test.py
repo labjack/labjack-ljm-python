f4118c2ea036607e61277a123ff6840325a01ede
David Lopez <david@labjack.com>
2016-10-21 18:04:26 -0600

    Started c-r_speed_test update for T4 support. Still need to finish.

--- Examples/Testing/c-r_speed_test.py
+++ Examples/Testing/c-r_speed_test.py
@@ -3,40 +3,65 @@ Performs LabJack operations in a loop and reports the timing statistics for the
 operations.
 
 """
+from datetime import datetime
+import functools
+import timeit
 
 from labjack import ljm
-from datetime import datetime
-import timeit
-import functools
+
 
 def eNamesIteration(handle, numFrames, names, aWrites, aNumValues, aValues,
                     results):
-    # Function for timit.Timer. Performs a eNames call to do LabJack operations.
-    # Takes eNames parameters and a list for results which will be filled.
+    """Function for timit.Timer. Performs an eNames call to do LabJack
+    operations. Takes eNames parameters and a list for results which
+    will be filled.
+
+    """
     del results[:]
     r = ljm.eNames(handle, numFrames, names, aWrites, aNumValues, aValues)
     results.extend(r)
 
+
 # Open first found LabJack
-handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")
-#handle = ljm.openS("ANY", "ANY", "ANY")
+handle = ljm.openS("ANY", "ANY", "ANY")  # Any device, Any connection, Any identifier
+#handle = ljm.openS("T7", "ANY", "ANY")  # T7 device, Any connection, Any identifier
+#handle = ljm.openS("T4", "ANY", "ANY")  # T4 device, Any connection, Any identifier
+#handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")  # Any device, Any connection, Any identifier
 
 info = ljm.getHandleInfo(handle)
-print("Opened a LabJack with Device type: %i, Connection type: %i,\n" \
-    "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" % \
-    (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
+print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
+      "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
+      (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
+deviceType = info[0]
 
-numIterations = 1000 # Number of iterations to perform in the loop
+numIterations = 1000  # Number of iterations to perform in the loop
 
 # Analog input settings
-numAIN = 1 # Number of analog inputs to read
-rangeAIN = 10.0
+numAIN = 1  # Number of analog inputs to read
 resolutionAIN = 1
 
 # Digital settings
 readDigital = False
 writeDigital = False
+
+# Device specific configuration
+if deviceType == ljm.constants.dtT4:
+    # T4 analog input configuration
+    rangeAINHV = 10.0  # HV channels range
+    rangeAINLV = 2.4  # LV channels range
+
+    # Configure the channels to analog input or digital I/O
+    # Update all digital I/O channels. b1 = Ignored. b = Affected.
+    dioInhibit = 0x00000  # (b00000000000000000000)
+    # Set AIN 0 to numAIN-1 as analog inputs (b1), the rest as digital I/O (b0).
+    dioAnalogEnable = (2 ** numAIN) - 1
+    ljm.eWriteNames(handle,
+                    ["DIO_INHIBIT", "DIO_ANALOG_ENABLE"],
+                    [dioInhibit, dioAnalogEnable])
+else:
+    # T7 and other devices analog input configuration
+    rangeAIN = 10.0
 
 # Analog output settings
 writeDACs = False
@@ -48,11 +73,18 @@ if numAIN > 0:
     aValues = []
     for i in range(numAIN):
         numFrames += 2
-        names.append("AIN%i_RANGE"%i)
-        aValues.append(rangeAIN)
-        names.append("AIN%i_RESOLUTION_INDEX"%i)
+        names.append("AIN%i_RANGE" % i)
+        if deviceType == ljm.constants.dtT4:
+            aValues.append(rangeAIN)
+        else:
+            if i < 4:
+                # Set the HV range
+                aValues.append(rangeAINHV)
+            else:
+                # Set the LV range
+                aValues.append(rangeAINLV)
+        names.append("AIN%i_RESOLUTION_INDEX" % i)
         aValues.append(resolutionAIN)
-
     ljm.eWriteNames(handle, numFrames, names, aValues)
 
 # Initialize and configure eNames parameters for loop's eNames call
@@ -65,7 +97,7 @@ aValues = []
 # Add analog input reads (AIN 0 to numAIN-1)
 for i in range(numAIN):
     numFrames += 1
-    names.append("AIN%i"%i)
+    names.append("AIN%i" % i)
     aWrites.append(ljm.constants.READ)
     aNumValues.append(1)
     aValues.append(0)
@@ -84,20 +116,18 @@ if writeDigital is True:
     names.append("DIO_STATE")
     aWrites.append(ljm.constants.WRITE)
     aNumValues.append(1)
-    aValues.append(0) #output-low
+    aValues.append(0)  # output-low
 
 if writeDACs is True:
     # Add analog output writes (DAC0-1)
     for i in range(2):
         numFrames += 1
-        names.append("DAC%i"%i)
+        names.append("DAC%i" % i)
         aWrites.append(ljm.constants.WRITE)
         aNumValues.append(1)
-        aValues.append(0.0) #0.0 V
-
+        aValues.append(0.0)  # 0.0 V
 
 print("\nTest frames:")
-
 for i in range(numFrames):
     if aWrites[i] == ljm.constants.READ:
         wrStr = "READ"
@@ -106,7 +136,6 @@ for i in range(numFrames):
     print("    %s %s" % (wrStr, names[i]))
 
 print("\nBeginning %i iterations..." % numIterations)
-
 
 # Initialize time variables
 maxMS = 0
@@ -118,7 +147,7 @@ t = timeit.Timer(functools.partial(eNamesIteration, handle, numFrames, names,
 
 # eNames loop
 for i in range(numIterations):
-    ttMS = t.timeit(number = 1)
+    ttMS = t.timeit(number=1)
     if minMS == 0:
         minMS = ttMS
     minMS = min(ttMS, minMS)
@@ -128,10 +157,9 @@ for i in range(numIterations):
 print("\n%i iterations performed:" % numIterations)
 print("    Time taken: %.3f ms" % (totalMS * 1000))
 print("    Average time per iteration: %.3f ms" %
-      (totalMS/numIterations * 1000))
-print("    Min / Max time for one iteration: %.3f ms / %.3f ms" % \
-        (minMS*1000, maxMS*1000))
-
+      (totalMS / numIterations * 1000))
+print("    Min / Max time for one iteration: %.3f ms / %.3f ms" %
+      (minMS * 1000, maxMS * 1000))
 print("\nLast eNames results: ")
 for i in range(numFrames):
     if aWrites[i] == ljm.constants.READ:
