ebddb5cf87ac8d0b21d1cfc6d63e6b4c6cbbc891
David Lopez <david@labjack.com>
2016-10-25 12:39:34 -0600

    Updated Watchdog examples for T4. Firmware doesn't support this feature yet, so test later.

--- Examples/Watchdog/read_watchdog_config.py
+++ Examples/Watchdog/read_watchdog_config.py
@@ -2,20 +2,21 @@
 Demonstrates how to read the Watchdog configuration from a LabJack.
 
 """
-
 from labjack import ljm
 
 
 # Open first found LabJack
-handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")
-#handle = ljm.openS("ANY", "ANY", "ANY")
+handle = ljm.openS("ANY", "ANY", "ANY")  # Any device, Any connection, Any identifier
+#handle = ljm.openS("T7", "ANY", "ANY")  # T7 device, Any connection, Any identifier
+#handle = ljm.openS("T4", "ANY", "ANY")  # T4 device, Any connection, Any identifier
+#handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")  # Any device, Any connection, Any identifier
 
 info = ljm.getHandleInfo(handle)
-print("Opened a LabJack with Device type: %i, Connection type: %i,\n" \
-    "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" % \
-    (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
+print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
+      "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
+      (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
-# Setup and call eReadNames to read the Watchdog config. from the LabJack.
+# Setup and call eReadNames to read the Watchdog configuration from the LabJack.
 aNames = ["WATCHDOG_ENABLE_DEFAULT", "WATCHDOG_ADVANCED_DEFAULT",
           "WATCHDOG_TIMEOUT_S_DEFAULT", "WATCHDOG_STARTUP_DELAY_S_DEFAULT",
           "WATCHDOG_STRICT_ENABLE_DEFAULT", "WATCHDOG_STRICT_KEY_DEFAULT",
--- Examples/Watchdog/write_watchdog_config.py
+++ Examples/Watchdog/write_watchdog_config.py
@@ -2,18 +2,19 @@
 Demonstrates how to configure the Watchdog on a LabJack.
 
 """
-
 from labjack import ljm
 
 
 # Open first found LabJack
-handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")
-#handle = ljm.openS("ANY", "ANY", "ANY")
+handle = ljm.openS("ANY", "ANY", "ANY")  # Any device, Any connection, Any identifier
+#handle = ljm.openS("T7", "ANY", "ANY")  # T7 device, Any connection, Any identifier
+#handle = ljm.openS("T4", "ANY", "ANY")  # T4 device, Any connection, Any identifier
+#handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")  # Any device, Any connection, Any identifier
 
 info = ljm.getHandleInfo(handle)
-print("Opened a LabJack with Device type: %i, Connection type: %i,\n" \
-    "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" % \
-    (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
+print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
+      "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
+      (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
 # Setup and call eWriteNames to configure the Watchdog on a LabJack.
 # Disable the Watchdog first before any other configuration.
@@ -26,13 +27,13 @@ aNames = ["WATCHDOG_ENABLE_DEFAULT", "WATCHDOG_ADVANCED_DEFAULT",
           "WATCHDOG_DAC0_DEFAULT", "WATCHDOG_DAC1_ENABLE_DEFAULT",
           "WATCHDOG_DAC1_DEFAULT", "WATCHDOG_ENABLE_DEFAULT"]
 aValues = [0, 0,
-           20, 0,
+           20, 0,  # Timeout set to 20 seconds
            0, 0,
-           1, 0,
+           1, 0,  # Reset the device on timeout
            0, 0,
            0, 0,
            0, 0,
-           0, 0] #Set WATCHDOG_ENABLE_DEFAULT to 1 to enable
+           0, 0]  # Set the last WATCHDOG_ENABLE_DEFAULT to 1 to enable
 numFrames = len(aNames)
 ljm.eWriteNames(handle, numFrames, aNames, aValues)
 
