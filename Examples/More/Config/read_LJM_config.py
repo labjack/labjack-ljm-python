b9a12ba40ad9dd4dac6ad5482506b97b653e1d37
Soloman J <soloman.jarman@gmail.com>
2022-03-29 10:40:11 -0600

    Removed device open from read LJM config example

--- Examples/More/Config/read_LJM_config.py
+++ Examples/More/Config/read_LJM_config.py
@@ -16,19 +16,6 @@ LJM Library:
 """
 from labjack import ljm
 
-
-# Open first found LabJack
-handle = ljm.openS("ANY", "ANY", "ANY")  # Any device, Any connection, Any identifier
-#handle = ljm.openS("T8", "ANY", "ANY")  # T8 device, Any connection, Any identifier
-#handle = ljm.openS("T7", "ANY", "ANY")  # T7 device, Any connection, Any identifier
-#handle = ljm.openS("T4", "ANY", "ANY")  # T4 device, Any connection, Any identifier
-#handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")  # Any device, Any connection, Any identifier
-
-info = ljm.getHandleInfo(handle)
-print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
-      "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
-      (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
-
 # Read the name of the error constants file being used by LJM
 configString = "LJM_ERROR_CONSTANTS_FILE"
 print("%s: %s" % (configString, ljm.readLibraryConfigStringS(configString)))
