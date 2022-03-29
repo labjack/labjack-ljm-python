5c74f192e8b5165e6f589ed0415e49f59a53c74a
Soloman J <soloman.jarman@gmail.com>
2022-03-29 10:45:23 -0600

    Fixed typo in read LJM config example

--- Examples/More/Config/read_LJM_config.py
+++ Examples/More/Config/read_LJM_config.py
@@ -20,7 +20,7 @@ from labjack import ljm
 configString = "LJM_ERROR_CONSTANTS_FILE"
 print("%s: %s" % (configString, ljm.readLibraryConfigStringS(configString)))
 
-# Write the communication send/recieve timeout for LJM
+# Write the communication send/receive timeout for LJM
 configString = "LJM_SEND_RECEIVE_TIMEOUT_MS"
 ljm.writeLibraryConfigS(configString, 5000)
 
