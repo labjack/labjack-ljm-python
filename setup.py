23f418d9204fdee379722487ab48dbea98f8669e
Ryker LabJack <ryker.labjack@gmail.com>
2023-12-04 15:05:09 -0700

    Added python 3.10 and 3.11 compatability

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
     'Topic :: Software Development',
     'Topic :: Software Development :: Embedded Systems',
     'Topic :: System :: Hardware'
