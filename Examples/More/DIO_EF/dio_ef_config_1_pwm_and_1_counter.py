2a9d760e5f6b82165f861e99f9b5f36c3d4d1227
Soloman J <soloman.jarman@gmail.com>
2024-01-26 16:45:08 -0700

    Updated comments on DIO_EF example

--- Examples/More/DIO_EF/dio_ef_config_1_pwm_and_1_counter.py
+++ Examples/More/DIO_EF/dio_ef_config_1_pwm_and_1_counter.py
@@ -62,7 +62,13 @@ if deviceType == ljm.constants.dtT4:
     # Use CIO2 (DIO18) for the high speed counter
     pwmDIO = 6
     counterDIO = 18
-    # Set FIO and EIO lines to digital I/O.
+    # T4 core frequency is 80 MHz
+    coreFreq = 80000000
+    # This next configuration should not be necessary unless you think you may
+    # have set FIO6 to analog (it needs to be set to digital).
+    # Inhibit all bits except bit6 (FIO6). Set the uninhibited DIO (only FIO6)
+    # to digital. If you intend to use bitmask registers or DIO_ANALOG_ENABLE
+    # later, be sure to reconfigure DIO_INHIBIT first.
     ljm.eWriteNames(handle, 2,
                     ["DIO_INHIBIT", "DIO_ANALOG_ENABLE"],
                     [0xFBF, 0x000])
@@ -71,34 +77,50 @@ elif deviceType == ljm.constants.dtT7:
     # Use CIO2 (DIO18) for the high speed counter
     pwmDIO = 0
     counterDIO = 18
-else:
+    # T7 core frequency is 80 MHz
+    coreFreq = 80000000
+elif deviceType == ljm.constants.dtT8:
     # For the T8, use FIO7 (DIO7) for the PWM output
     # Use FIO6 (DIO6) for the high speed counter
     pwmDIO = 7
     counterDIO = 6
+    # T8 core frequency is 100 MHz
+    coreFreq = 100000000
 
-aNames = ["DIO_EF_CLOCK0_DIVISOR", "DIO_EF_CLOCK0_ROLL_VALUE",
+pwmFreq = 10000 # output frequency (also the number of pulses per second)
+dutyCycle = 25 # % duty cycle
+clockDivisor = 1 # DIO_EF_CLOCK#_DIVISOR
+
+clockFreq = coreFreq / clockDivisor # DIO_EF_CLOCK frequency
+rollValue = clockFreq / pwmFreq # DIO_EF_CLOCK#_ROLL_VALUE
+configA = dutyCycle * rollValue / 100 # DIO#_EF_CONFIG_A
+
+
+aNames = ["DIO_EF_CLOCK0_ENABLE",
+          "DIO_EF_CLOCK0_DIVISOR", "DIO_EF_CLOCK0_ROLL_VALUE",
           "DIO_EF_CLOCK0_ENABLE", "DIO%i_EF_ENABLE" % pwmDIO,
           "DIO%i_EF_INDEX" % pwmDIO, "DIO%i_EF_CONFIG_A" % pwmDIO,
           "DIO%i_EF_ENABLE" % pwmDIO, "DIO%i_EF_ENABLE" % counterDIO,
           "DIO%i_EF_INDEX" % counterDIO, "DIO%i_EF_ENABLE" % counterDIO]
-aValues = [1, 8000,
-           1, 0,
-           0, 2000,
-           1, 0,
-           7, 1]
+aValues = [0, # Disable the DIO_EF clock
+           clockDivisor, rollValue, # Set PWM clock divisor and roll
+           1, 0, # Enable the clock and disable any features on the PWM DIO
+           0, configA, # Set the PWM feature index and duty cycle (configA)
+           1, 0, # Enable the PWM and disable any features on the counter DIO
+           7, 1] # Set the counter feature index and enable the counter.
 numFrames = len(aNames)
 results = ljm.eWriteNames(handle, numFrames, aNames, aValues)
 
 # Wait 1 second.
 time.sleep(1.0)
 
-# Read from the counter.
+# Read from the counter. Since we waited 1 second, we expect the value read to
+# be close to the PWM frequency (10000 pulses per second)
 value = ljm.eReadName(handle, "DIO%i_EF_READ_A" %counterDIO)
 
 print("\nCounter = %f" % (value))
 
-# Turn off PWM output and counter
+# Disable the DIO_EF clock, PWM output, and counter.
 aNames = ["DIO_EF_CLOCK0_ENABLE", "DIO%i_EF_ENABLE" % pwmDIO,
           "DIO%i_EF_ENABLE" % counterDIO]
 aValues = [0, 0, 0]
