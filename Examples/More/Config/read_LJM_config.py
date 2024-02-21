f76d69d1f25069433668d74cc56908bfe33e7611
David Lopez <dave.l.lopez@gmail.com>
2024-02-21 16:33:51 -0700

    Added exception note and changed end lines to CRLF in read_LJM_config.py.

--- Examples/More/Config/read_LJM_config.py
+++ Examples/More/Config/read_LJM_config.py
@@ -13,8 +13,18 @@ LJM Library:
     Library configuration functions:
         https://labjack.com/support/software/api/ljm/function-reference/library-configuration-functions
 
+Note:
+    Our Python interfaces throw exceptions when there are any issues with
+    device communications that need addressed. Many of our examples will
+    terminate immediately when an exception is thrown. The onus is on the API
+    user to address the cause of any exceptions thrown, and add exception
+    handling when appropriate. We create our own exception classes that are
+    derived from the built-in Python Exception class and can be caught as such.
+    For more information, see the implementation in our source code and the
+    Python standard documentation.
 """
 from labjack import ljm
+
 
 # Read the name of the error constants file being used by LJM
 configString = "LJM_ERROR_CONSTANTS_FILE"
