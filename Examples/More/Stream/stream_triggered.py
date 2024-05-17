f81c3f2d568fc4f671325267fd7c6b579c7b8c9f
David Lopez <dave.l.lopez@gmail.com>
2024-05-17 15:36:40 -0600

    Removed semicolons in stream_triggered.py code.

--- Examples/More/Stream/stream_triggered.py
+++ Examples/More/Stream/stream_triggered.py
@@ -94,16 +94,16 @@ def configureDeviceForTriggeredStream(handle, triggerName):
     @type triggerName: str
     """
     address = ljm.nameToAddress(triggerName)[0]
-    ljm.eWriteName(handle, "STREAM_TRIGGER_INDEX", address);
+    ljm.eWriteName(handle, "STREAM_TRIGGER_INDEX", address)
 
     # Clear any previous settings on triggerName's Extended Feature registers
-    ljm.eWriteName(handle, "%s_EF_ENABLE" % triggerName, 0);
+    ljm.eWriteName(handle, "%s_EF_ENABLE" % triggerName, 0)
 
     # 5 enables a rising or falling edge to trigger stream
-    ljm.eWriteName(handle, "%s_EF_INDEX" % triggerName, 5);
+    ljm.eWriteName(handle, "%s_EF_INDEX" % triggerName, 5)
 
     # Enable
-    ljm.eWriteName(handle, "%s_EF_ENABLE" % triggerName, 1);
+    ljm.eWriteName(handle, "%s_EF_ENABLE" % triggerName, 1)
 
 
 def configureLJMForTriggeredStream():
