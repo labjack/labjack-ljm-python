1a7232f3ece7f4d40baffe8091e1c872a9d51659
David Lopez <dave.l.lopez@gmail.com>
2024-06-07 17:28:50 -0600

    Updated stream_external_clock.py example.

--- Examples/More/Stream/stream_external_clock.py
+++ Examples/More/Stream/stream_external_clock.py
@@ -1,6 +1,8 @@
 '''
-Shows how to stream with the T7 or T8 in external clock stream mode.
-Note: Similar to stream_callback.py, which uses a callback to read from stream.
+Demonstrates how to stream with the T7 or T8 in external clock stream mode.
+Connect CIO3 (T7) or FIO2 (T8) to FIO3 to use Pulse Out for the stream clock.
+
+Note: The T4 is not supported in this example.
 
 Relevant Documentation:
 
@@ -11,13 +13,17 @@ LJM Library:
         https://labjack.com/support/software/api/ljm
     Opening and Closing:
         https://labjack.com/support/software/api/ljm/function-reference/opening-and-closing
-    Utility Functions (such as NamesToAddresses):
-        https://labjack.com/support/software/api/ljm/function-reference/utility
-    Stream Functions:
+    NamesToAddresses:
+        https://labjack.com/support/software/api/ljm/function-reference/utility/ljmnamestoaddresses
+    eWriteName:
+        https://labjack.com/support/software/api/ljm/function-reference/ljmewritename
+    eWriteNames:
+        https://labjack.com/support/software/api/ljm/function-reference/ljmewritenames
+    Stream Functions (such as eStreamStart, eStreamRead and eStreamStop):
         https://labjack.com/support/software/api/ljm/function-reference/stream-functions
-    Constants:
-        https://labjack.com/support/software/api/ljm/constants
-    Library Configuration Functions:
+    eStreamStart (for eStreamStart and externally clock stream in LJM)
+        https://labjack.com/support/ljm/users-guide/function-reference/ljmestreamstart
+    Library Configuration Functions (such as WriteLibraryConfigS and stream parameters):
         https://labjack.com/support/software/api/ljm/function-reference/library-configuration-functions
 
 T-Series and I/O:
@@ -25,175 +31,60 @@ T-Series and I/O:
         https://labjack.com/support/software/api/modbus/modbus-map
     Stream Mode:
         https://labjack.com/support/datasheets/t-series/communication/stream-mode
+    Special Stream Modes (such as externally clocked):
+        https://support.labjack.com/docs/3-2-2-special-stream-modes-t-series-datasheet
     Analog Inputs:
         https://labjack.com/support/datasheets/t-series/ain
-    Stream-Out:
-        https://labjack.com/support/datasheets/t-series/communication/stream-mode/stream-out/stream-out-description
     Digital I/O:
         https://labjack.com/support/datasheets/t-series/digital-io
+    Hardware Overview (such as register CORE_TIMER):
+        https://labjack.com/support/datasheets/t-series/hardware-overview
     Pulse Out:
         https://labjack.com/support/datasheets/t-series/digital-io/extended-features/pulse-out
-    Stream Mode (externally clocked):
-        https://labjack.com/support/datasheets/t-series/communication/stream-mode#externally-clocked
-    Hardware Overview(Device Information Registers):
-        https://labjack.com/support/datasheets/t-series/hardware-overview
+
+Note:
+    Our Python interfaces throw exceptions when there are any issues with
+    device communications that need addressed. Many of our examples will
+    terminate immediately when an exception is thrown. The onus is on the API
+    user to address the cause of any exceptions thrown, and add exception
+    handling when appropriate. We create our own exception classes that are
+    derived from the built-in Python Exception class and can be caught as such.
+    For more information, see the implementation in our source code and the
+    Python standard documentation.
 '''
 import sys
 from datetime import datetime
-import ljm_stream_util
+from time import sleep
+
 from labjack import ljm
 
-def HardcodedConfigureStream(handle):
 
-    STREAM_TRIGGER_INDEX = 0
-    STREAM_RESOLUTION_INDEX = 0
-    STREAM_SETTLING_US = 0
-    AIN_ALL_RANGE = 0
-    AIN_ALL_NEGATIVE_CH = ljm.constants.GND
+def variableStreamSleep(scansPerRead, scanRate, LJMScanBacklog):
+    """Sleeps for approximately the expected amount of time until the next scan
+    is ready to be read.
 
-    print("Writing configurations:")
-
-    if STREAM_TRIGGER_INDEX == 0:
-        print("    Ensuring triggered stream is disabled:")
-
-    print("    Setting STREAM_TRIGGER_INDEX to %d" % STREAM_TRIGGER_INDEX)
-    ljm.eWriteName(handle, "STREAM_TRIGGER_INDEX", STREAM_TRIGGER_INDEX)
-
-    # Configure the analog inputs' negative channel, range, settling time and
-    # resolution.
-    # Note: when streaming, negative channels and ranges can be configured for
-    # individual analog inputs, but the stream has only one settling time and
-    # resolution. The settling time and negative channel configurations do not
-    # do anything on the T8 due to the isolated and simultaneously sampled AIN.
-    print("    Setting STREAM_RESOLUTION_INDEX to %d" %
-        STREAM_RESOLUTION_INDEX)
-    ljm.eWriteName(handle, "STREAM_RESOLUTION_INDEX", STREAM_RESOLUTION_INDEX)
-
-    print("    Setting STREAM_SETTLING_US to %f" % STREAM_SETTLING_US)
-    ljm.eWriteName(handle, "STREAM_SETTLING_US", STREAM_SETTLING_US)
-
-    print("    Setting AIN_ALL_RANGE to %f" % AIN_ALL_RANGE)
-    ljm.eWriteName(handle, "AIN_ALL_RANGE", AIN_ALL_RANGE)
-
-    print("    Setting AIN_ALL_NEGATIVE_CH to %s" %
-        "LJM_GND" if AIN_ALL_NEGATIVE_CH == ljm.constants.GND
-        else AIN_ALL_NEGATIVE_CH)
-
-    print("\n")
-    ljm.eWriteName(handle, "AIN_ALL_NEGATIVE_CH", AIN_ALL_NEGATIVE_CH)
-
-
-def EnableFIO0PulseOut(handle, pulseRate, numPulses):
-    # Set FIO0 to do a 50% duty cycle
-    # https://labjack.com/support/datasheets/t-series/digital-io/extended-features/pulse-out
-
-    rollValue = 10000000/pulseRate
-
-    print("\nEnabling %d pulses on FIO0 at a %d Hz pulse rate\n" % (numPulses, pulseRate))
-
-    ljm.eWriteName(handle, "DIO0_EF_ENABLE", 0)
-    ljm.eWriteName(handle, "DIO_EF_CLOCK0_DIVISOR", 8)
-    ljm.eWriteName(handle, "DIO_EF_CLOCK0_ROLL_VALUE", int(rollValue))
-    ljm.eWriteName(handle, "DIO_EF_CLOCK0_ENABLE", 1)
-    ljm.eWriteName(handle, "DIO0_EF_INDEX", 2)
-    ljm.eWriteName(handle, "DIO0_EF_OPTIONS", 0)
-    ljm.eWriteName(handle, "DIO0", 0)
-    ljm.eWriteName(handle, "DIO0_EF_CONFIG_A", int(rollValue/2))
-    ljm.eWriteName(handle, "DIO0_EF_CONFIG_B", 0)
-    ljm.eWriteName(handle, "DIO0_EF_CONFIG_C", int(numPulses))
-    ljm.eWriteName(handle, "DIO0_EF_ENABLE", 1)
-
-
-
-def StreamReturnAllOrNone(handle):
-
-    FIO0PulseOut = 1
-    maxRequests = 10
-
-    # Variables for LJM_eStreamStart
-    scanRate = 1000
-    scansPerRead = scanRate/2
-
-    # Configure LJM for unpredictable stream timing
-    ljm.writeLibraryConfigS(ljm.constants.STREAM_SCANS_RETURN,
-        ljm.constants.STREAM_SCANS_RETURN_ALL_OR_NONE)
-    ljm.writeLibraryConfigS(ljm.constants.STREAM_RECEIVE_TIMEOUT_MODE,
-        ljm.constants.STREAM_RECEIVE_TIMEOUT_MODE_MANUAL)
-    ljm.writeLibraryConfigS(ljm.constants.STREAM_RECEIVE_TIMEOUT_MS, 100)
-
-    aNames = ["AIN0","AIN1","AIN2","AIN3"]
-    numAddresses = len(aNames)
-    aScanList = ljm.namesToAddresses(numAddresses, aNames)[0]
-
-    HardcodedConfigureStream(handle)
-
-    print("Starting externally clocked stream...")
-    # Set external clock source on CIO3
-    ljm.eWriteName(handle, "STREAM_CLOCK_SOURCE", 2)
-    ljm.eWriteName(handle, "STREAM_EXTERNAL_CLOCK_DIVISOR", 1)
-
-    if FIO0PulseOut == 1:
-        EnableFIO0PulseOut(handle, scanRate, scanRate*maxRequests + 5000)
-
-    ljm.eStreamStart(handle, int(scansPerRead), numAddresses, aScanList, scanRate)
-    i = 1
-    ljmScanBacklog = 0
-    totScans = 0
-    totSkip = 0
-
-    start = datetime.now()
-    while i <= maxRequests:
-        ljm_stream_util.variableStreamSleep(scansPerRead, scanRate, ljmScanBacklog)
-
-        try:
-            ret = ljm.eStreamRead(handle)
-
-            aData = ret[0]
-            ljmScanBacklog = ret[2]
-            scans = len(aData) / numAddresses
-            totScans += scans
-
-            # Count the skipped samples which are indicated by -9999 values. Missed
-            # samples occur after a device's stream buffer overflows and are
-            # reported after auto-recover mode ends.
-            curSkip = aData.count(-9999.0)
-            totSkip += curSkip
-
-            print("\neStreamRead %i" % i)
-            ainStr = ""
-            for j in range(0, numAddresses):
-                ainStr += "%s = %0.5f, " % (aNames[j], aData[j])
-            print("  1st scan out of %i: %s" % (scans, ainStr))
-            print("  Scans Skipped = %0.0f, Scan Backlogs: Device = %i, LJM = "
-                  "%i" % (curSkip/numAddresses, ret[1], ljmScanBacklog))
-            i += 1
-        except ljm.LJMError as err:
-            if err.errorCode == ljm.errorcodes.NO_SCANS_RETURNED:
-                sys.stdout.write('.')
-                sys.stdout.flush()
-                continue
-            else:
-                raise err
-    end = datetime.now()
-
-    print("\nTotal scans = %i" % (totScans))
-    tt = (end - start).seconds + float((end - start).microseconds) / 1000000
-    print("Time taken = %f seconds" % (tt))
-    print("LJM Scan Rate = %f scans/second" % (scanRate))
-    print("Timed Scan Rate = %f scans/second" % (totScans / tt))
-    print("Timed Sample Rate = %f samples/second" % (totScans * numAddresses / tt))
-    print("Skipped scans = %0.0f" % (totSkip / numAddresses))
-
+    @para scansPerRead: The number of scans returned by a eStreamRead call.
+    @type scansPerRead: int
+    @para scanRate: The stream scan rate.
+    @type scanRate: numerical
+    @para LJMScanBacklog: The number of backlogged scans in the LJM buffer.
+    @type LJMScanBacklog: int
+    """
+    DECREASE_TOTAL = 0.9
+    sleepFactor = 0.0
+    portionScansReady = float(LJMScanBacklog) / scansPerRead
+    if portionScansReady <= DECREASE_TOTAL:
+        sleepFactor = (1 - portionScansReady) * DECREASE_TOTAL
+    sleepTime = sleepFactor * scansPerRead / float(scanRate)
+    sleep(sleepTime)
 
 
 if __name__ == "__main__":
-
     try:
         # Open first found LabJack
         handle = ljm.openS("ANY", "ANY", "ANY")  # Any device, Any connection, Any identifier
         #handle = ljm.openS("T8", "ANY", "ANY")  # T8 device, Any connection, Any identifier
         #handle = ljm.openS("T7", "ANY", "ANY")  # T7 device, Any connection, Any identifier
-        #handle = ljm.openS("T4", "ANY", "ANY")  # T4 device, Any connection, Any identifier
         #handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")  # Any device, Any connection, Any identifier
 
         info = ljm.getHandleInfo(handle)
@@ -208,12 +99,156 @@ if __name__ == "__main__":
             print("Exiting now.")
             exit(1)
 
-        # Disable stream if enabled
-        if ljm.eReadName(handle, "STREAM_ENABLE") == 1:
-            ljm.eWriteName(handle, "STREAM_ENABLE", 0)
+        # Configure negative channels and stream settling.
+        # Not applicable to the T8.
+        if deviceType == ljm.constants.dtT7:
+            # All negative channels are single-ended.
+            # Stream settling is 0 (default).
+            aNames = ["AIN_ALL_NEGATIVE_CH", "STREAM_SETTLING_US"]
+            aValues = [ljm.constants.GND, 0]
+            numFrames = len(aNames)
+            ljm.eWriteNames(handle, numFrames, aNames, aValues)
 
-        StreamReturnAllOrNone(handle)
+        # Ensure triggered stream is disabled.
+        # AIN ranges are set to ±10V (T7) or ±11V (T8).
+        # Stream resolution index is 0 (default).
+        aNames = ["STREAM_TRIGGER_INDEX", "AIN_ALL_RANGE",
+                  "STREAM_RESOLUTION_INDEX"]
+        aValues = [0, 10.0,
+                   0]
+        numFrames = len(aNames)
+        ljm.eWriteNames(handle, numFrames, aNames, aValues)
 
+        # Stream Configuration
+        FIO3_CLOCK = True  # Use Pulse Out on FIO3 for the stream clock
+        NUM_LOOP_ITERATIONS = 10
+        scanRate = 1000  # Scans per second
+        scansPerRead = scanRate/2  # Number of scans returned by eStreamRead call
+        aScanListNames = ["AIN0", "FIO_STATE", "CORE_TIMER", "STREAM_DATA_CAPTURE_16"]  # Scan list names to stream.
+        numAddresses = len(aScanListNames)
+        aScanList = ljm.namesToAddresses(numAddresses, aScanListNames)[0]  # Scan list addresses to stream. eStreamStart uses Modbus addresses.
+
+        # Configure LJM for unpredictable stream timing.
+        ljm.writeLibraryConfigS(ljm.constants.STREAM_SCANS_RETURN,
+            ljm.constants.STREAM_SCANS_RETURN_ALL_OR_NONE)
+        ljm.writeLibraryConfigS(ljm.constants.STREAM_RECEIVE_TIMEOUT_MODE,
+            ljm.constants.STREAM_RECEIVE_TIMEOUT_MODE_MANUAL)
+        ljm.writeLibraryConfigS(ljm.constants.STREAM_RECEIVE_TIMEOUT_MS, 100)
+
+        # Configure stream clock source to external clock source on
+        # CIO3 for the T7, or FIO2 for the T8.
+        print("\nSetting up externally clocked stream.\n")
+        ljm.eWriteName(handle, "STREAM_CLOCK_SOURCE", 2)
+        ljm.eWriteName(handle, "STREAM_EXTERNAL_CLOCK_DIVISOR", 1)
+
+        if FIO3_CLOCK == True:
+            # Enable Pulse Out with frequency of 1 kHz and 50% duty
+            # cycle on FIO3.
+            pulseRate = scanRate
+            numPulses = scanRate*NUM_LOOP_ITERATIONS + 5000
+
+            # Set the clock divisor so that the clock frequency is 10 MHz.
+            if deviceType == ljm.constants.dtT8:
+                # ClockFrequency = 100 MHz / 4 = 25 MHz
+                # PulseOutFrequency = 25 MHz / 25 KHz = 1 KHz
+                clockDivisor = 4
+                rollValue = 25000
+            else:
+                # ClockFrequency = 80 MHz / 8 = 10 MHz
+                # PulseOutFrequency = 10 MHz / 10 KHz = 1 KHz
+                clockDivisor = 8
+                rollValue = 10000
+
+            # DutyCycle% = 100 * CONFIG_A / RollValue
+            # CONFIG_A = DutyCycle% / 100 * RollValue
+            dutyCyclePercent = 50
+            dutyCycleValue = dutyCyclePercent / 100 * rollValue
+
+            print("Enabling %d pulses on FIO3 at a %d Hz pulse rate.\n" % (numPulses, pulseRate))
+
+            ljm.eWriteName(handle, "DIO3_EF_ENABLE", 0)
+            ljm.eWriteName(handle, "DIO3", 0)
+            ljm.eWriteName(handle, "DIO_EF_CLOCK0_DIVISOR", clockDivisor)
+            ljm.eWriteName(handle, "DIO_EF_CLOCK0_ROLL_VALUE", rollValue)
+            ljm.eWriteName(handle, "DIO_EF_CLOCK0_ENABLE", 1)
+            ljm.eWriteName(handle, "DIO3_EF_INDEX", 2)
+            ljm.eWriteName(handle, "DIO3_EF_CLOCK_SOURCE", 0)
+            ljm.eWriteName(handle, "DIO3_EF_CONFIG_A", dutyCycleValue)
+            ljm.eWriteName(handle, "DIO3_EF_CONFIG_B", 0)
+            ljm.eWriteName(handle, "DIO3_EF_CONFIG_C", numPulses)
+            ljm.eWriteName(handle, "DIO3_EF_ENABLE", 1)
+
+        # Configure and start stream
+        scanRate = ljm.eStreamStart(handle, int(scansPerRead), numAddresses, aScanList, scanRate)
+        ljmScanBacklog = 0
+        deviceScanBacklog = 0
+        totScans = 0
+        totSkip = 0
+
+        start = datetime.now()
+        i = 0
+        while i < NUM_LOOP_ITERATIONS:
+            try:
+                variableStreamSleep(scansPerRead, scanRate, ljmScanBacklog)
+
+                ret = ljm.eStreamRead(handle)
+
+                aData = ret[0]
+                deviceScanBacklog = ret[1]
+                ljmScanBacklog = ret[2]
+                scans = len(aData) / numAddresses
+                totScans += scans
+
+                # Count the skipped samples which are indicated by -9999 values.
+                # Missed samples occur after a device's stream buffer overflows
+                # and are reported after auto-recover mode ends.
+                curSkip = aData.count(-9999.0)
+                totSkip += curSkip
+                i += 1
+
+                print("\neStreamRead %i" % i)
+
+                # Parse out first scan of samples to display.
+                # CORE_TIMER and STREAM_DATA_CAPTURE_16 samples
+                # are combined for the full 32-bit CORE_TIMER value.
+                scanStr = ""
+                coreTimer32bits = 0
+                for j in range(numAddresses):
+                    if j == 2:
+                        # Sample 2 = CORE_TIMER
+                        # CORE_TIMER is the lower 16-bits of the full
+                        # 32-bit CORE_TIMER value.
+                        coreTimer32bits = aData[j]
+                    elif j == 3:
+                        # Sample 3 = STREAM_DATA_CAPTURE_16
+                        # STREAM_DATA_CAPTURE_16 is the upper 16-bits of the
+                        # full 32-bit CORE_TIMER value.
+                        coreTimer32bits += int(aData[j]) << 16
+                        scanStr += "CORE_TIMER and STREAM_DATA_CAPTURE_16 = %d" % (coreTimer32bits)
+                    else:
+                        # Sample 0 = AIN0
+                        # Sample 1 = FIO_STATE
+                        scanStr += "%s = %0.5f, " % (aScanListNames[j], aData[j])
+
+                print("  1st scan out of %i: %s" % (scans, scanStr))
+                print("  Scans Skipped = %0.0f, Scan Backlogs: Device = %i, "
+                    "LJM = %i" % (curSkip/numAddresses, deviceScanBacklog, ljmScanBacklog))
+            except ljm.LJMError as err:
+                if err.errorCode == ljm.errorcodes.NO_SCANS_RETURNED:
+                    sys.stdout.write('.')
+                    sys.stdout.flush()
+                    continue
+                else:
+                    raise err
+        end = datetime.now()
+
+        print("\nTotal scans = %i" % (totScans))
+        tt = (end - start).seconds + float((end - start).microseconds) / 1000000
+        print("Time taken = %f seconds" % (tt))
+        print("LJM Scan Rate = %f scans/second" % (scanRate))
+        print("Timed Scan Rate = %f scans/second" % (totScans / tt))
+        print("Timed Sample Rate = %f samples/second" % (totScans * numAddresses / tt))
+        print("Skipped scans = %0.0f" % (totSkip / numAddresses))
     except ljm.LJMError:
         ljme = sys.exc_info()[1]
         print(ljme)
