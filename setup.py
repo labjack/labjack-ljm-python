8bed686371ef02c0eef80bbfcf62dbedcb63fd62
David Lopez <dave.l.lopez@gmail.com>
2025-03-31 12:52:11 -0600

    Updated CLASSIFIERS to include Python 3.12 and 3.13.

--- setup.py
+++ setup.py
@@ -22,6 +22,8 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
     'Topic :: Software Development',
     'Topic :: Software Development :: Embedded Systems',
     'Topic :: System :: Hardware'
