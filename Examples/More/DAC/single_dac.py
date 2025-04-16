17e6ab1588824ad89cce7e10ba9825e6fb6a3df1
David Lopez <dave.l.lopez@gmail.com>
2025-04-16 14:09:38 -0600

    Added the single_dac.py example.

---
+++ Examples/More/DAC/single_dac.py
@@ -0,0 +1,57 @@
+"""
+Demonstrates setting a single DAC on a LabJack.
+
+Relevant Documentation:
+
+LJM Library:
+    LJM Library Installer:
+        https://labjack.com/support/software/installers/ljm
+    LJM Users Guide:
+        https://labjack.com/support/software/api/ljm
+    Opening and Closing:
+        https://labjack.com/support/software/api/ljm/function-reference/opening-and-closing
+    eWriteName:
+        https://labjack.com/support/software/api/ljm/function-reference/ljmewritename
+    Constants:
+        https://labjack.com/support/software/api/ljm/constants
+
+T-Series and I/O:
+    Modbus Map:
+        https://labjack.com/support/software/api/modbus/modbus-map
+    DAC:
+        https://labjack.com/support/datasheets/t-series/dac
+
+Note:
+    Our Python interfaces throw exceptions when there are any issues with
+    device communications that need addressed. Many of our examples will
+    terminate immediately when an exception is thrown. The onus is on the API
+    user to address the cause of any exceptions thrown, and add exception
+    handling when appropriate. We create our own exception classes that are
+    derived from the built-in Python Exception class and can be caught as such.
+    For more information, see the implementation in our source code and the
+    Python standard documentation.
+"""
+from labjack import ljm
+
+
+# Open first found LabJack
+handle = ljm.openS("ANY", "ANY", "ANY")  # Any device, Any connection, Any identifier
+#handle = ljm.openS("T8", "ANY", "ANY")  # T8 device, Any connection, Any identifier
+#handle = ljm.openS("T7", "ANY", "ANY")  # T7 device, Any connection, Any identifier
+#handle = ljm.openS("T4", "ANY", "ANY")  # T4 device, Any connection, Any identifier
+#handle = ljm.open(ljm.constants.dtANY, ljm.constants.ctANY, "ANY")  # Any device, Any connection, Any identifier
+
+info = ljm.getHandleInfo(handle)
+print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
+      "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
+      (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
+
+# Setup and call eWriteName to set DAC0's voltage.
+name = "DAC0"
+value = 3.5  # 3.5 V
+ljm.eWriteName(handle, name, value)
+
+print("\n%s set to %0.3f V." % (name, value))
+
+# Close handle
+ljm.close(handle)
